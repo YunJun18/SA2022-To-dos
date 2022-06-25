3dd941ca47cddd62fa79e6ca1dbe8a950e741b64
Tan Yun Jun <yunjun09@rafflesgirlssch.edu.sg>
2022-06-25 10:15:36 +0800

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by T Krobot on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
